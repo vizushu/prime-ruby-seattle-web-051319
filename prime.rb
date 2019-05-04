a2e91a006af1a7bda8a3525409849dd21487cbdb
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-04 17:06:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,7 @@
-def prime?(*nums)
-    nums.each do |num|
-    i = 2
-      while i < num
-        if num % i == 0
-          puts true
-        else
-          puts false
-        end
-        i += 1
-      end
-   end
-end
+def prime?(num)
+  return false if num <= 1
+  for i in 2..(num/2)
+    return false if num % i == 0
+  end
+  true
+end 
