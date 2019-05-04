04d081245ea468c45df2dc5d431b261a3fd0a878
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-04 17:03:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,3 +2,17 @@ def prime?(number)
     return false if [0, 1].include?(number)
     (2...number).none? { |n| number % n == 0}
 end
+
+def prime?(nums)
+    nums.each do |num|
+    i = 2
+      while i < num
+        if num % i == 0
+          puts "#{num} is not a prime"
+        else
+          puts "#{num} is a prime"
+        end
+        i += 1
+      end
+   end
+end
